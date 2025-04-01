f9ecec9de95d8d970ebc0bca2204fd3d2c19c6c5
gabrielly.roris <gabrielly.roris@gmail.com>
2025-04-01 01:02:05 +0000

    adaptação da troca de card ao escolher uma nota da avaliação

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ export function App() {
   }
  
   return (
-    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl
+    notaAvaliacao === 0 ? (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl
     font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
         <img src={iconStar} alt="icon star" />
@@ -37,8 +38,16 @@ export function App() {
           rounded-full text-medium-grey text-sm font-bold" onClick={() =>
             handleMudarNotaAvaliacao(5)}/>
         </div>
+        
         <button className="bg-orange w-full uppercase tracking-1 font-bold
         rounded-3xl text-sm py-3">Submit</button>
       </div>
+
+    ) : (
+       <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl
+    font-overpass">
+      <p>{notaAvaliacao}</p>
+      </div>
+    )
   )
 }
