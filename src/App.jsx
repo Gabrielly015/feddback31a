87d8194c8aad369e0433a44f26bcd3a04653531e
gabrielly.roris <gabrielly.roris@gmail.com>
2025-04-22 01:36:35 +0000

    criação do componente para o card final e ajuste do transition dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import illustrationthankyou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 import { CardInicial } from "./components/CardInicial"
+import { CardFinal } from "./components/CardFinal"
 
 export function App(){
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
@@ -10,12 +10,7 @@ export function App(){
     submited === false ? (
         <CardInicial  setNotaAvaliacao={setNotaAvaliacao}  notaAvaliacao={notaAvaliacao} setSubmited={setSubmited} />
       ) : (
-        <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
-          <img className="mx-auto mb-6" src={illustrationthankyou} alt="illustration thank you" />
-          <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {notaAvaliacao} out of 5</p>
-          <h1 className="text-2xl font-bold mb-2.5 text-center">Thank You!</h1>
-          <p className= "text-sm text-light-grey leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
-        </div>
+      <CardFinal notaAvaliacao={notaAvaliacao} />
       )
     )
   }
